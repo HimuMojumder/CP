a324c0ea18e2d7e303cc6d78835cfa2540b156a8
HimuMojumder <himu3272@gmail.com>
2025-11-02 01:55:36 +0600

    Add files via upload

---
+++ String/String_To_Word.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+      string s;
+    getline(cin,s);
+    vector<string>v;
+    string word;
+    stringstream ss(s);
+    while(ss>>word){
+        v.push_back(word);
+    }
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
