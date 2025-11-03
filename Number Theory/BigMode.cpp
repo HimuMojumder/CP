c92bbd405a4efa15551d7ef3d3c8ea284b20cbe5
HimuMojumder <himu3272@gmail.com>
2025-11-03 18:08:25 +0600

    Update BigMode.cpp

--- Number Theory/BigMode.cpp
+++ Number Theory/BigMode.cpp
@@ -4,7 +4,7 @@ int mod=1e9+7;
 int bigmode(int a,int b){
     if(b==0)return 1;
     if(b%2){
-        return (a%mod+bigmode(a,b-1)%mod)%mod;
+        return (a%mod*bigmode(a,b-1)%mod)%mod;
     }else{
       int tem=bigmode(a,b/2)%mod;
     return (tem*tem)%mod;
@@ -27,3 +27,4 @@ int main(){
     return 0;
 
 }
+
