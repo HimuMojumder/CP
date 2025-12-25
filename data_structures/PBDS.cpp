fc2cc41a142ff993f1deecb67e39b98b14e96848
HimuMojumder <himu3272@gmail.com>
2025-12-25 11:18:36 +0600

    Update PBDS.cpp

--- data_structures/PBDS.cpp
+++ data_structures/PBDS.cpp
@@ -3,11 +3,14 @@
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
 template<typename T> using ordered_set =tree<T, null_type,less<T>, rb_tree_tag,tree_order_statistics_node_update>;
-//order_of_key (k) : Number of items strictly smaller than k .
-//find_by_order(k) : K-th element in a set (counting from zero).
+//order_of_key (k) : Number of items strictly smaller than k . syntax -> os.order_of_key  -> thats return integer
+
+//find_by_order(k) : K-th element in a set (counting from zero). syntax -> os.find_by_order -> thats return iterator/ address
+
 template<typename T> using ordered_multiset = tree<T, null_type,less_equal<T>, rb_tree_tag,tree_order_statistics_node_update>;
 //Erase -
 //if(os.upper_bound(x)!=os.end())
  //   os.erase(os.upper_bound(x));
 //gp_hash_table
+
 
