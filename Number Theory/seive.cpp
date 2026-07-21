7917d3bf90453959dfc1e8ccaf973377282b6319
HimuMojumder <himu3272@gmail.com>
2026-07-21 21:25:05 +0600

    Update seive.cpp

--- Number Theory/seive.cpp
+++ Number Theory/seive.cpp
@@ -28,4 +28,37 @@ signed main(){
     for(int i=0;i<ans.size();i+=100){
         cout<<ans[i]<<"\n";
     }
-}       
+}    
+
+
+
+//seive using bitset up to 1e7
+
+#include<bits/stdc++.h>
+using namespace std;
+
+//seive using bitset
+const int N=1e8+9;
+bitset<N>f;
+
+int32_t main(){
+   ios_base ::sync_with_stdio(0);
+   cin.tie(0);
+   int n=N-9;
+   vector<int>primes;
+   f[1]=true;
+   for(int i=2;i*i<=n;i++){
+       if(!f[i]){
+           for(int j=i*i;j<=n;j+=i){
+              f[j]=true;
+           }
+       }
+   }
+   for(int i=2;i<=n;i++){
+    if(!f[i])primes.push_back(i);
+   }
+   
+   cout<<primes.size()<<endl;
+
+   return 0;
+}
